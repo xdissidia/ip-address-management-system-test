d6262788a80b0893b809e8b711ebcf0fab6e9023
name <jayfructuoso@gmail.com>
2024-03-30 14:42:13 +0800

    chore(frontend): clear nextjs template structure

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -2,19 +2,28 @@ import { Inter } from "next/font/google";
 import { Roboto } from "next/font/google";
 
 import "@styles/globals.css";
+import Nav from "./components/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
 export const metadata = {
-  title: "IP Address Management Information System",
-  description: "IP Address Management Information System",
+    title: "IP Address Management Information System",
+    description: "IP Address Management Information System",
 };
 
+const isLoggedIn = true;
+
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={roboto.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <div className="main">
+                <div className="gradient" />
+            </div>
+            <main className={isLoggedIn ? "app" : "app min-h-screen"} >
+                {isLoggedIn && <Nav />}
+                {children}
+            </main>
+        </html>
+    );
 }
