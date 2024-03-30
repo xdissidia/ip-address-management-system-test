8b1c7d53e2bba69cf8f49b1b7a026545704cb479
name <jayfructuoso@gmail.com>
2024-03-30 18:18:05 +0800

    feat(audit_trails): Add audit trail for LOGIN and LOGOUT methods

--- api/app/Http/Controllers/Auth/UserLoginController.php
+++ api/app/Http/Controllers/Auth/UserLoginController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
-use Illuminate\Http\Request;
+use App\Models\AuditTrail;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserLoginController extends Controller
@@ -18,12 +19,19 @@ class UserLoginController extends Controller
         $request->authenticate();
 
         $user = Auth::user();
+
+        AuditTrail::create([
+            'ip_address' => $request->getClientIp(),
+            'action' => 'LOGIN',
+            'description' => "{$user->name} has logged in.",
+        ])->user()->associate($user)->save();
+
         return response()->json([
             'user' => $user,
             'authorization' => [
                 'token' => $user->createToken('ApiToken')->plainTextToken,
                 'type' => 'bearer',
-            ]
+            ],
         ]);
 
     }
@@ -33,7 +41,16 @@ class UserLoginController extends Controller
      */
     public function destroy(Request $request): JsonResponse
     {
-        Auth::user()->tokens()->delete();
+
+        $user = Auth::user();
+        $user->tokens()->delete();
+
+        AuditTrail::create([
+            'ip_address' => $request->getClientIp(),
+            'action' => 'LOGOUT',
+            'description' => "{$user->name} has logged out.",
+        ])->user()->associate($user)->save();
+
         return response()->json([
             'message' => 'Successfully logged out',
         ]);
