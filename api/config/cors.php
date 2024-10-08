0f5dc0c43c0576a7be476def4eb619bfe7482ea0
root <root@root.root>
2024-10-08 09:45:33 +0800

    up

--- api/config/cors.php
+++ api/config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://10.100.100.135:3000')],
 
-    'allowed_origins_patterns' => ['*'],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
